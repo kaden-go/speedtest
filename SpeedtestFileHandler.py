85a5c074525733335606944fae8d3a4361d8f855
Kevin Aden <kevin.aden@pm.me>
2021-07-03 11:57:30 +0200

    add gitignore

--- SpeedtestFileHandler.py
+++ SpeedtestFileHandler.py
@@ -1,14 +1,13 @@
 import csv
 from datetime import date, datetime
 
-header = ['downstream [Mbit/s]', 'upstream [Mbit/s]', 'ping [ms]', 'timestamp']
-
 class SpeedtestFileHandler():
 
     def __init__(self):
         self.__file = None
         self.__writer = None
         self.__init = False
+        self.__header = ['downstream [Mbit/s]', 'upstream [Mbit/s]', 'ping [ms]', 'timestamp']
         self.__write_header()
 
     def __open_file(self):
@@ -21,7 +20,7 @@ class SpeedtestFileHandler():
 
     def __write_header(self):
         self.__open_file()
-        self.writer.writerow(header)
+        self.writer.writerow(self.__header)
         self.__close_file()
 
     def write_data(self, data):
